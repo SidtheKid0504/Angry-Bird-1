60e7eee2b3a77f6320cde9feb74414444ac1fe1f
SidtheKid0504 <63013935+SidtheKid0504@users.noreply.github.com>
2020-06-29 12:59:02 -0400

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,26 @@
+class Pig {
+    constructor(x,y) {
+        var pig_options = {
+            'restitution': 0.8,
+            'friction': 0.5,
+            'density': 1
+        }
+
+        this.body = Bodies.rectangle(x, y, 50, 50, pig_options);
+        this.width = 50;
+        this.height = 50;
+        World.add(world, this.body)
+    }
+
+    display() {
+        var angle = this.body.angle;
+        var pos = this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("red");
+        rect(0, 0, this.width, this.height);
+        pop();
+    }
+}
